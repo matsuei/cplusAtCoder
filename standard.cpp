ad5517634cabf86114204138d8d28a74a96d61e1
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-26 10:32:05 +0900

    Update standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -5,11 +5,15 @@ int main() {
   int n = 0;
   cin >> n;
   int d = 0;
-  vector<int> a(n);
-  for (int i { 0 }; i < n; ++i) {
+  vector<int> a(0);
+  for (int i = 0; i < n; ++i) {
     cin >> d;
     a.push_back(d);
   }
+  vector<int> a(n), b(n), c(n);
+  rep(i, n) cin >> a[i];
+  rep(i, n) cin >> b[i];
+  rep(i, n) cin >> c[i];
   cout << s << endl;  // *
   cout << a << endl;  // 1
 }
