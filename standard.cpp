44ad5c672eb5731dca61ef6ffbe6cb725ca3c422
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-26 09:12:33 +0900

    Update standard.cpp

--- standard.cpp
+++ standard.cpp
@@ -3,15 +3,14 @@ using namespace std;
  
 int main() {
     int N = 0;
-	cin >> N;
-
-    int d = 0;
-	vector<int> D(N);
-	for (int i { 0 }; i < N; ++i) {
-		cin >> d;
-		D.push_back(d);
-	}
+    cin >> N;
     
-  cout << s << endl;  // *
-  cout << a << endl;  // 1
+    int d = 0;
+    vector<int> D(N);
+    for (int i { 0 }; i < N; ++i) {
+        cin >> d;
+        D.push_back(d);
+    }    
+    cout << s << endl;  // *
+    cout << a << endl;  // 1
 }
