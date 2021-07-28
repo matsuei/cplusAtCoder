2fb319e3a0bdb6059826228dcd13a5695a0a4b7a
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-07-28 20:59:24 +0900

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -1,1 +1,16 @@
-
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+ 
+int main() {
+  string s;
+  cin >> s;
+  
+  string pre = s.substr(0, n);
+  string post = s.substr(n);
+  
+  // 参照先の入れ替え
+  swap(pre[a], pre[b])
+  swap(pre, post)
+  cout << pre << post << endl;
+}
