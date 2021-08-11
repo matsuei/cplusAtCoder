070712c7c249f7a5f24441182d82e1e0f558669b
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-08-11 19:49:04 +0900

    Update digit.cpp

--- digit.cpp
+++ digit.cpp
@@ -1,1 +1,15 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+
+int main() {
+  long long n;
+  cin >> n;
+  vector<int> digits;
+  while (n) {
+    digits.push_back(n % 10);
+    n /= 10;
+  }
+}
+
 
