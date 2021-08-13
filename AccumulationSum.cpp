5e2abfed128855f563323c45c7ec0c772ab6a23c
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-08-13 22:00:47 +0900

    Update AccumulationSum.cpp

--- AccumulationSum.cpp
+++ AccumulationSum.cpp
@@ -1,1 +1,16 @@
-
+int main() {
+  int n;
+  cin >> n;
+  
+  vector<int> a(0);
+  int d = 0;
+  for (int i = 0; i < n; ++i) {
+    cin >> d;
+    a.push_back(d);
+  }
+  vector<int> b(0);
+  b.push_back(0);
+  for (int i = 1; i < n + 1; ++i) {
+    b.push_back(a[i - 1] - b[i - 1]);
+  }
+}
