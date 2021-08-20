3cc49e0c2ce23224319cbb62740b7a4f71b07549
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-08-20 21:08:31 +0900

    Update string.cpp

--- string.cpp
+++ string.cpp
@@ -13,17 +13,10 @@ int main() {
   swap(pre[a], pre[b])
   swap(pre, post)
   cout << pre << post << endl;
- 
- // 1文字ずつ
- string str = "abcde";
-  for (int i = 0; i < w; ++i) {
-    for (int j = 0; j < h; ++j) {
-      string ch = str.substr(j, 1);
-        if (ch == "#") {
-          data.at(i).at(j) = 1;
-        } else {
-          data.at(i).at(j) = 0;
-        }
-    }
+  
+  // 1文字ずつ
+  string str = "abcde";
+  for (int i = 0; i < str.size(); ++i) {
+    s.at(i);
   }
 }
