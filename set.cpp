8cd65d9a7ecf605911b0bc1519c89f4b73335c63
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-09-11 18:27:23 +0900

    Update set.cpp

--- set.cpp
+++ set.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++.h>
 
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  int key;
+  set<int> cut;
+  
+  // key以下の値を持つイテレータを取得
+  decltype(cut)::iterator it = cut.lower_bound(key);
+  if (it == cut.begin()) {
+    // 先頭の要素
+  } else if (it == cut.end()) {
+    /// 該当の要素なし
+  } else {
+    // 先頭以外の要素
+  }
+  return 0;
+}
