748c91e055909071db501194ec1d15282e441901
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-09-28 21:40:40 +0900

    Update map.cpp

--- map.cpp
+++ map.cpp
@@ -4,3 +4,10 @@ rep(i, 0, N) cin >> A[i];
 //　最初のintをキーに、2番目のintを管理
 map<int, int> cnt;
 rep(i, 0, N) cnt[A[i]]++;
+
+// cntの全要素を回す
+vector<int> count(0);
+  for (auto p : cnt)
+  {
+    count.push_back(p.second);
+  }
