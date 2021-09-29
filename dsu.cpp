4463f2a16e9a309a7498da6a3a9bea9dc4ffd8c9
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-09-29 20:37:40 +0900

    Update dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,1 +1,32 @@
+//https://atcoder.github.io/ac-library/production/document_ja/dsu.html
+
+#include <bits/stdc++.h>
+#include <atcoder/all>
+using namespace std;
+using namespace atcoder;
+
+int main(){
+  int n,m;
+  cin>>n>>m;
+  
+  //n頂点のグラフgを用意する
+  dsu g(n);
+  
+  for(int i=0;i<m;i++){
+    int a,b;
+    cin>>a>>b;
+    
+    //入力の1がgでの0に対応
+    a--;b--;
+    
+    //辺を追加する
+    g.merge(a,b);
+  }
+ 
+  //連結成分の数-1が答え
+  int ans=g.groups().size()-1;
+  
+  cout<<ans<<endl;
+}
+
 
