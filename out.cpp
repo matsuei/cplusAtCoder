5d24b96333b3ba9ca3d674d12f9d0bf06ae5d816
Matsue Kenta <40712363+matsuei@users.noreply.github.com>
2021-10-22 20:35:55 +0900

    Update out.cpp

--- out.cpp
+++ out.cpp
@@ -1,1 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+  
+  // 配列の中身をスペース区切りで出力
+  for (int i = 0; i < vec.size()-1; i ++){
+    cout << vec.at(i) << " ";
+  }
+  cout << vec.back() << endl;
+}
